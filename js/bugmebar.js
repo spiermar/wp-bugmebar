a14cdf339daca2745ef50822d79ec05ebcad20d1
Martin Spier <spiermar@gmail.com>
2014-11-22 20:59:30 -0800

    Decreasing cookie timeout to 1 day.

--- js/bugmebar.js
+++ js/bugmebar.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function () {
     jQuery('#navigation').bugme({
     	message: 'Check my latest post, <a id="bugmelink" href="/performance-capacity-cmg-2014">"Performance and Capacity by CMG 2014"</a>!',
     	align: "center",
+    	remember: true,
+        expireIn: 1
     });
     jQuery('#bugmelink').click(function () {
     	_trackEvent("BugMeBar", "Click");
