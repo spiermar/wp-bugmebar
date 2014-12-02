e5b9a4e70b77f61032ed3e2f639f8a16e5cf73ea
Martin Spier <spiermar@gmail.com>
2014-12-01 20:38:51 -0800

    Double quotes.

--- js/bugmebar.js
+++ js/bugmebar.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function () {
     jQuery('#navigation').bugme({
-    	message: 'Check my latest post, <a id="bugmelink" href="/performance-capacity-cmg-2014">"Performance and Capacity by CMG 2014"</a>!',
+    	message: 'Check my latest post, "<a id="bugmelink" href="/performance-capacity-cmg-2014">Performance and Capacity by CMG 2014</a>"!',
     	align: "center",
     	remember: true,
         expireIn: 1
